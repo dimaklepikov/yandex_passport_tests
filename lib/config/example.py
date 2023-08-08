afb51736dca090197c10b036c13b8dcb9187289d
Dmitriy Klepikov <klepikovdima@gmail.com>
2023-08-08 15:33:03 +0300

    Fix typo

--- lib/config/example.py
+++ lib/config/example.py
@@ -6,8 +6,8 @@ parser = ConfigParser()
 if __name__ == "__main__":
     parser.read()
 
-    a = parser.config.sections()
     info = parser.get_section("Info")
+
     print("Get value by key 'version':")
     print(parser.get_value(info, "version"))
     print("Get value by key 'author':")
